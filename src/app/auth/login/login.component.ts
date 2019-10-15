d3d1603e8461042d2b089d1cbe5af2ff8d951ddb
JyothiMamidi <jyothi.mamidi@quantela.com>
2019-10-15 17:07:59 +0530

    login ts changes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,7 +9,7 @@ import { noop, Observable } from 'rxjs';
 import { TouchSequence } from 'selenium-webdriver';
 import { isloggedIn, isloggedOut } from '../auth.selectors';
 import { Router } from '@angular/router';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 
 
@@ -23,17 +23,24 @@ export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService ,
               private store: Store<AppState>,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder : FormBuilder) { }
  username = 'jyothimamidi@gmail.com';
  password = '123456';
 
  isloggedIn$ : Observable<boolean>;
  isloggedOut$ : Observable<boolean>;
   ngOnInit() {
-    this.isloggedIn$ = this.store.
+  this.loginForm = this.formBuilder.group({
+    email : new FormControl(null, [Validators.required, Validators.email]),
+    password :new FormControl(null, Validators.required),
+    remember :new FormControl(false)
+  });
+
+  this.isloggedIn$ = this.store.
       select(isloggedIn);
 
-    this.isloggedOut$ = this.store.select(isloggedOut);
+  this.isloggedOut$ = this.store.select(isloggedOut);
   }
 
   onLogin() {
@@ -63,6 +70,27 @@ export class LoginComponent implements OnInit {
     this.router.navigateByUrl('login');
   }
   onSubmit() {
-this.router.navigateByUrl('');
+//this.router.navigateByUrl('');
+console.log(this.loginForm);
+
+this.authService.Login(this.username, this.password).
+pipe(
+  tap(
+    user => {
+      this.store.dispatch(new Login({user}));
+    }
+  )
+).
+subscribe(
+  noop,
+  () => {
+    const user: User =  {
+      id: '123445rffd',
+      email : 'jyothimamidi@gmail.com'
+     };
+    this.store.dispatch(new Login({user}));
+  //  alert('please enter valid username or password');
+  }
+);
   }
 }
