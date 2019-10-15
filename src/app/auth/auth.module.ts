476cb1e07f9de049d0f9b5ff03f380d75e7d965c
JyothiMamidi <jyothi.mamidi@quantela.com>
2019-10-15 15:59:52 +0530

    style changes and footer

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -20,7 +20,7 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-{path: '', component: RegisterComponent},
+{path: '', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
 {path: 'login', component: LoginComponent}
 ];
