276c45141e419033aa626449c580dd531b8710c1
NaveenaCh <naveena.chokkara@quantela.com>
2019-11-01 10:49:29 +0530

    dashboard-reports component

--- src/app/home/dashboard/dashboard.module.ts
+++ src/app/home/dashboard/dashboard.module.ts
@@ -5,16 +5,22 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MapviewComponent } from './mapview/mapview.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
+import { ReportsComponent } from './reports/reports.component';
+
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent }
 ];
 
 
 @NgModule({
-  imports: [FlexLayoutModule,LeafletModule,CommonModule],
-declarations:[DashboardComponent, MapviewComponent],
-providers:[],
-exports:[DashboardComponent,MapviewComponent]
+  imports: [FlexLayoutModule, LeafletModule, CommonModule, MatFormFieldModule, MatTabsModule],
+  declarations: [DashboardComponent, MapviewComponent, ReportsComponent],
+  providers: [],
+  exports: [DashboardComponent, MapviewComponent, ReportsComponent]
 })
 
 export class DashboardModule {
---
+++ src/app/home/dashboard/reports/reports.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-reports',
+  templateUrl: './reports.component.html'
+})
+export class ReportsComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
