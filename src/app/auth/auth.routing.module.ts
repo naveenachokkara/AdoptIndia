6d95fb7d287845e8f64b4866aee5189af242bfd5
NaveenaCh <naveena.chokkara@quantela.com>
2019-11-12 11:05:36 +0530

    auth routing changes

--- src/app/auth/auth.routing.module.ts
+++ src/app/auth/auth.routing.module.ts
@@ -5,18 +5,21 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthComponent } from './auth.component';
 
-const authRoutes : Routes = [
-  {path: '', component: AuthComponent , children:[
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
-  ]}
-  ];
+const authRoutes: Routes = [
+  {
+    path: '', component: AuthComponent, children: [
+      { path: '', redirectTo: 'register', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  }
+];
 
 @NgModule({
-imports:[
-  RouterModule.forChild(authRoutes)
-],
-exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(authRoutes)
+  ],
+  exports: [RouterModule]
 })
 
 export class AuthRoutingModule {
