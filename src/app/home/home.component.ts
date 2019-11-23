28d3050a4900d42c5764531d9e66aa2ef3989fe8
NaveenaCh <naveena.chokkara@quantela.com>
2019-11-23 09:12:03 +0530

    legend box shadow and notification functionality changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Logout } from '../auth/auth.actions';
 import { AuthState } from '../auth/auth.reducer';
+import { MatMenu } from '@angular/material';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,16 @@ export class HomeComponent implements OnInit {
   show = true;
   sidenavWidth = 4;
   mainbodyWidth = 96;
+  notifications = ['dummy noification 1', 'notification 2', 'notification 3'];
+  count = this.notifications.length;
 
   constructor(private router: Router, private store: Store<AuthState>) { }
 
   ngOnInit() {
+  }
+
+  onNotiMenuClick() {
+    this.count = 0;
   }
 
   increase() {
