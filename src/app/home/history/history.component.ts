6fc750fd2ed51712a718e51a7cb30f9f32a530f7
NaveenaCh <naveena.chokkara@quantela.com>
2019-11-28 11:53:34 +0530

    reset filter functionality

--- src/app/home/history/history.component.ts
+++ src/app/home/history/history.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatInput, MatDatepicker } from '@angular/material';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'app-history',
@@ -8,11 +10,34 @@ import { FormControl } from '@angular/forms';
 })
 export class HistoryComponent implements OnInit {
   tabs = [{ name: 'History', route: './' }, { name: 'Drivers', route: './drivers' }, { name: 'Vehicles', route: './vehicles' }];
+  date = null;
+  selectedreporttype = null;
+  selectedvehicletype = null;
+
+  @ViewChild('fromdate', {
+    read: MatInput
+  }) fromdate: MatInput;
+
+  @ViewChild('todate', {
+    read: MatInput
+  }) todate: MatInput;
+
   constructor() { }
 
   ngOnInit() {
-   const date = new FormControl(new Date());
-   const serializedDate = new FormControl((new Date()).toISOString());
+    this.date = new FormControl(new Date());
+    const serializedDate = new FormControl((new Date()).toISOString());
+  }
+
+  onResetFilter() {
+    this.fromdate.value = '';
+    this.todate.value = '';
+    this.selectedreporttype = null;
+    this.selectedvehicletype = null;
+  }
+
+  onSearch(){
+    
   }
 
 }
